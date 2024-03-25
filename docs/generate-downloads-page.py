3cb9d5013f144e2ee0f8c2b3b7479313491773d1
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2024-03-25 18:45:36 +0100

    generate-downloads-page.py - Draft (5)

--- docs/generate-downloads-page.py
+++ docs/generate-downloads-page.py
@@ -2,8 +2,8 @@
 
 import os
 import shutil
-import subprocess
 import re
+import requests
 
 # Please run from docs folder
 
@@ -11,6 +11,8 @@ downloads_md='downloads-PRUEBA-DE-CONCEPTO.md'
 templatesDir='templates'
 imagesDir="images"
 repoReleasesTagUrl="https://github.com/maldua/zimbra-foss-builder/releases/tag"
+
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
 def append_files(file1_path, file2_path):
     with open(file1_path, 'r') as file1:
@@ -47,31 +49,8 @@ def get_download_row (prefixTag, versionTag, distroLongName, tgzDownloadUrl, bui
 
 ## Main loop
 
-# Get all of the repo tags (tagsMatrix)
-# Keep only tags that start with: 'zimbra-foss-build-'
-# Save into a matrix:
-# - tag: zimbra-foss-build-ubuntu-20.04/9.0.0.p39
-# - buildDate
-
-pGitTag = subprocess.Popen(['git', 'tag', '--format=%(creatordate:unix)%09%(refname:strip=2)', '--sort=-taggerdate'], stdout=subprocess.PIPE,text=True)
-
-dateRegex = re.compile('(.*)\t.*')
-tagRegex = re.compile('.*\t(.*)')
-
-
-tagsMatrix = []
-
-for line in pGitTag.stdout:
-    dateFound = re.findall(dateRegex, line)[0]
-    tagFound = re.findall(tagRegex, line)[0]
-    tagsItem = {}
-    tagsItem["tag"] = tagFound
-    tagsItem["date"] = dateFound
-    tagsMatrix.append(tagsItem)
-
-print(tagsMatrix)
-
 # Get info from releases based on previous tag matrix (releasesMatrix)
+# Keep only tags that start with: 'zimbra-foss-build-'
 # - tag: zimbra-foss-build-ubuntu-20.04/9.0.0.p39
 # - buildDate
 # - prefixTag: zimbra-foss-build-ubuntu-20.04
@@ -79,6 +58,52 @@ print(tagsMatrix)
 # - distroLongName: Ubuntu 20.04 based on release title
 # - tgzDownloadUrl: https://...tgz based on assets which start with 'zcs-' and end in 'tgz'
 # - category: 'stable, beta, experimental, other' based on draft, pre-release values (use a helper function)
+
+response = requests.get("https://api.github.com/repos/maldua/zimbra-foss-builder/releases", headers={"Accept":"application/vnd.github+json", "Authorization":f"Bearer {GITHUB_TOKEN}", "X-GitHub-Api-Version":"2022-11-28"})
+responseJson = response.json()
+
+wantedTagRegex = re.compile('^zimbra-foss-build-.*$')
+prefixTagRegex = re.compile('(.*)/.*')
+versionTagRegex = re.compile('.*/(.*)')
+distroLongNameRegex =re.compile('.* \( (.*) \)')
+
+releasesMatrix = []
+
+for njson in responseJson:
+  tag = njson["tag_name"]
+  print (njson)
+  print ("")
+  if re.match(wantedTagRegex, tag):
+
+    prefixTag = re.findall(prefixTagRegex, tag)[0]
+    versionTag = re.findall(versionTagRegex, tag)[0]
+    distroLongName = re.findall(distroLongNameRegex, njson["name"])[0]
+    print (njson["name"])
+
+    tagsItem = {}
+    tagsItem["tag"] = tag
+    tagsItem["buildDate"] = njson["created_at"]
+    tagsItem["prefixTag"] = prefixTag
+    tagsItem["versionTag"] = versionTag
+    tagsItem["distroLongName"] = distroLongName
+
+    releasesMatrix.append(tagsItem)
+
+# print(releasesMatrix)
+
+# for line in pGitTag.stdout:
+#   dateFound = re.findall(dateRegex, line)[0]
+#   tagFound = re.findall(tagRegex, line)[0]
+#   # if re.match(wantedTagRegex, tagFound):
+#   if True:
+#     tagsItem = {}
+#     tagsItem["tag"] = tagFound
+#     tagsItem["buildDate"] = dateFound
+#     releasesMatrix.append(tagsItem)
+# 
+# for nTagsItem in releasesMatrix:
+#   print (nTagsItem["tag"])
+#   print (nTagsItem["buildDate"])
 
 # Create stableReleasesMatrix based on releasesMatrix
 # Create betaReleasesMatrix based on releasesMatrix
