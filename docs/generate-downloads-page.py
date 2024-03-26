5a022912fefb89b9371be6ebe33628b99484e8ae
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2024-03-26 16:40:26 +0100

    generate-downloads-page.py - Draft (14)

--- docs/generate-downloads-page.py
+++ docs/generate-downloads-page.py
@@ -15,11 +15,7 @@ repoReleasesTagUrl="https://github.com/maldua/zimbra-foss-builder/releases/tag"
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
-def append_files(file1_path, file2_path):
-    with open(file1_path, 'r') as file1:
-        with open(file2_path, 'a') as file2:
-            shutil.copyfileobj(file1, file2)
-
+# Download markdown functions
 def getIconField(prefixTag):
   if ("ubuntu" in prefixTag):
     iconField = f"![Ubuntu icon]({imagesDir}/ubuntu.png)"
@@ -46,63 +42,62 @@ def get_download_row (prefixTag, versionTag, distroLongName, tgzDownloadUrl, bui
   download_row = f"|{icon} | {distroLongName} | [64bit x86]({tgzDownloadUrl}) [(MD5)]({md5DownloadUrl}) [(SHA 256)]({sha256DownloadUrl}) | {buildDate} | [Build/Release details]({moreInformationUrl}) |"
   return (download_row)
 
-## Main loop
+# Releases Matrix functions
+def getReleasesMatrix():
+  # Get info from releases based on previous tag matrix (releasesMatrix)
+  # Keep only tags that start with: 'zimbra-foss-build-'
+  # - tag: zimbra-foss-build-ubuntu-20.04/9.0.0.p39
+  # - buildDate
+  # - prefixTag: zimbra-foss-build-ubuntu-20.04
+  # - versionTag: 9.0.0.p39
+  # - distroLongName: Ubuntu 20.04 based on release title
+  # - tgzDownloadUrl: https://...tgz based on assets which start with 'zcs-' and end in 'tgz'
+  # - category: 'stable, beta, experimental, other' based on draft, pre-release values (use a helper function)
 
-# Get info from releases based on previous tag matrix (releasesMatrix)
-# Keep only tags that start with: 'zimbra-foss-build-'
-# - tag: zimbra-foss-build-ubuntu-20.04/9.0.0.p39
-# - buildDate
-# - prefixTag: zimbra-foss-build-ubuntu-20.04
-# - versionTag: 9.0.0.p39
-# - distroLongName: Ubuntu 20.04 based on release title
-# - tgzDownloadUrl: https://...tgz based on assets which start with 'zcs-' and end in 'tgz'
-# - category: 'stable, beta, experimental, other' based on draft, pre-release values (use a helper function)
+  response = requests.get("https://api.github.com/repos/maldua/zimbra-foss-builder/releases", headers={"Accept":"application/vnd.github+json", "Authorization":f"Bearer {GITHUB_TOKEN}", "X-GitHub-Api-Version":"2022-11-28"})
+  responseJson = response.json()
 
-response = requests.get("https://api.github.com/repos/maldua/zimbra-foss-builder/releases", headers={"Accept":"application/vnd.github+json", "Authorization":f"Bearer {GITHUB_TOKEN}", "X-GitHub-Api-Version":"2022-11-28"})
-responseJson = response.json()
+  wantedTagRegex = re.compile('^zimbra-foss-build-.*$')
+  prefixTagRegex = re.compile('(.*)/.*')
+  versionTagRegex = re.compile('.*/(.*)')
+  distroLongNameRegex =re.compile('.* \( (.*) \)')
+  tgzRegex = re.compile('^zcs-.*tgz$')
 
-wantedTagRegex = re.compile('^zimbra-foss-build-.*$')
-prefixTagRegex = re.compile('(.*)/.*')
-versionTagRegex = re.compile('.*/(.*)')
-distroLongNameRegex =re.compile('.* \( (.*) \)')
-tgzRegex = re.compile('^zcs-.*tgz$')
+  releasesMatrix = []
 
-releasesMatrix = []
+  for nJson in responseJson:
+    tag = nJson["tag_name"]
+    # print (nJson)
+    # print ("")
+    if re.match(wantedTagRegex, tag):
 
-for nJson in responseJson:
-  tag = nJson["tag_name"]
-  # print (nJson)
-  # print ("")
-  if re.match(wantedTagRegex, tag):
+      prefixTag = re.findall(prefixTagRegex, tag)[0]
+      versionTag = re.findall(versionTagRegex, tag)[0]
+      distroLongName = re.findall(distroLongNameRegex, nJson["name"])[0]
 
-    prefixTag = re.findall(prefixTagRegex, tag)[0]
-    versionTag = re.findall(versionTagRegex, tag)[0]
-    distroLongName = re.findall(distroLongNameRegex, nJson["name"])[0]
+      tagsItem = {}
+      tagsItem["tag"] = tag
+      tagsItem["buildDate"] = nJson["created_at"]
+      tagsItem["prefixTag"] = prefixTag
+      tagsItem["versionTag"] = versionTag
+      tagsItem["distroLongName"] = distroLongName
 
-    tagsItem = {}
-    tagsItem["tag"] = tag
-    tagsItem["buildDate"] = nJson["created_at"]
-    tagsItem["prefixTag"] = prefixTag
-    tagsItem["versionTag"] = versionTag
-    tagsItem["distroLongName"] = distroLongName
+      if ( (nJson["prerelease"] == False) and (nJson["draft"] == False) ):
+        tagsItem["category"] = "stable"
+      elif ( (nJson["prerelease"] == True) and (nJson["draft"] == False) ):
+        tagsItem["category"] = "beta"
+      elif ( (nJson["prerelease"] == True) and (nJson["draft"] == True) ):
+        tagsItem["category"] = "experimental"
+      else:
+        tagsItem["category"] = "other"
 
-    if ( (nJson["prerelease"] == False) and (nJson["draft"] == False) ):
-      tagsItem["category"] = "stable"
-    elif ( (nJson["prerelease"] == True) and (nJson["draft"] == False) ):
-      tagsItem["category"] = "beta"
-    elif ( (nJson["prerelease"] == True) and (nJson["draft"] == True) ):
-      tagsItem["category"] = "experimental"
-    else:
-      tagsItem["category"] = "other"
+      for nAsset in nJson["assets"]:
+        if re.match(tgzRegex, nAsset["name"]):
+          tagsItem["tgzDownloadUrl"] = nAsset["browser_download_url"]
+          break
 
-    for nAsset in nJson["assets"]:
-      if re.match(tgzRegex, nAsset["name"]):
-        tagsItem["tgzDownloadUrl"] = nAsset["browser_download_url"]
-        break
-
-    releasesMatrix.append(tagsItem)
-
-# print(releasesMatrix)
+      releasesMatrix.append(tagsItem)
+  return (releasesMatrix)
 
 def filterByCategory(matrix, category):
   newMatrix = []
@@ -111,13 +106,7 @@ def filterByCategory(matrix, category):
       newMatrix.append(nRow)
   return (newMatrix)
 
-stableReleasesMatrix = filterByCategory(matrix=releasesMatrix, category="stable")
-betaReleasesMatrix = filterByCategory(matrix=releasesMatrix, category="beta")
-experimentalReleasesMatrix = filterByCategory(matrix=releasesMatrix, category="experimental")
-otherReleasesMatrix = filterByCategory(matrix=releasesMatrix, category="other")
-
-# print(betaReleasesMatrix)
-
+# Tag functions
 def getVersionTags(matrix):
   versionTags = []
   for nRow in matrix:
@@ -140,26 +129,18 @@ def orderedAndUniqueVersionTags (versionTags):
   versionTagsOrdered=(sortVersionProcess.stdout).rstrip().split('\n')
   return (versionTagsOrdered)
 
-stableVersionTags = getVersionTags (stableReleasesMatrix)
-stableVersionTags = orderedAndUniqueVersionTags (stableVersionTags)
-
-betaVersionTags = getVersionTags (betaReleasesMatrix)
-betaVersionTags = orderedAndUniqueVersionTags (betaVersionTags)
-
-experimentalVersionTags = getVersionTags (experimentalReleasesMatrix)
-experimentalVersionTags = orderedAndUniqueVersionTags (experimentalVersionTags)
-
-otherVersionTags = getVersionTags (otherReleasesMatrix)
-otherVersionTags = orderedAndUniqueVersionTags (otherVersionTags)
-
-# print (betaVersionTags)
-
 def filterByVersionTag(matrix, versionTag):
   newMatrix = []
   for nRow in matrix:
     if (nRow["versionTag"] == versionTag):
       newMatrix.append(nRow)
   return (newMatrix)
+
+# File output functions
+def append_files(file1_path, file2_path):
+    with open(file1_path, 'r') as file1:
+        with open(file2_path, 'a') as file2:
+            shutil.copyfileobj(file1, file2)
 
 def outputSection(downloads_md, versionTags, releasesMatrix, shortName):
   for nTagVersion in versionTags:
@@ -179,9 +160,34 @@ def outputNewLine(downloads_md):
   with open(downloads_md, 'a') as outfile:
     outfile.write('\n')
 
+# Get the main releasesMatrix with all of the releases information
+releasesMatrix = getReleasesMatrix()
+
+# Get our four main matrices
+stableReleasesMatrix = filterByCategory(matrix=releasesMatrix, category="stable")
+betaReleasesMatrix = filterByCategory(matrix=releasesMatrix, category="beta")
+experimentalReleasesMatrix = filterByCategory(matrix=releasesMatrix, category="experimental")
+otherReleasesMatrix = filterByCategory(matrix=releasesMatrix, category="other")
+
+# Get ordered (and unique) tags
+stableVersionTags = getVersionTags (stableReleasesMatrix)
+stableVersionTags = orderedAndUniqueVersionTags (stableVersionTags)
+
+betaVersionTags = getVersionTags (betaReleasesMatrix)
+betaVersionTags = orderedAndUniqueVersionTags (betaVersionTags)
+
+experimentalVersionTags = getVersionTags (experimentalReleasesMatrix)
+experimentalVersionTags = orderedAndUniqueVersionTags (experimentalVersionTags)
+
+otherVersionTags = getVersionTags (otherReleasesMatrix)
+otherVersionTags = orderedAndUniqueVersionTags (otherVersionTags)
+
+# Empty our output file
 if (os.path.isfile(downloads_md)):
   os.remove(downloads_md)
-#
+
+# Write the different sections as needed
+
 append_files(templatesDir + "/" + "downloads-top.md", downloads_md)
 append_files(templatesDir + "/" + "downloads-index.md", downloads_md)
 
