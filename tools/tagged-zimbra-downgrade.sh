f20cd931b540a81b5dd780d9599d8e3ea9f2923b
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2024-05-24 15:53:25 +0200

    Added tagged-zimbra-downgrade.sh (4)

--- tools/tagged-zimbra-downgrade.sh
+++ tools/tagged-zimbra-downgrade.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LDAP_DOWNGRADE_FIX_DIR="/opt/zimbra/conf/scripts/LDAP_DOWNGRADE_FIX"
+
 function print_10.0.7_config {
 
   cat << __EOF
@@ -696,9 +698,31 @@ function applyDowngrade {
 
   ldapmodify -x -H $ldap_master_url -D "uid=zimbra,cn=admins,cn=zimbra" -w $zimbra_ldap_password -f ${_LDAP_ENTRIES_TO_DELETE_LDIF_FILE}
 
-  ldap stop
-  zmcontrol start
+}
 
+function fixBorkedLdap {
+
+  mkdir -p ${LDAP_DOWNGRADE_FIX_DIR}
+  /opt/zimbra/libexec/zmslapcat ${LDAP_DOWNGRADE_FIX_DIR}
+  echo 'You can ignore xxxxxxx UNKNOWN attributeDescription "ZIMBRAxxxxxxxxxxxx" inserted. messages from above.'
+
+  # Actual fix
+  cp ${LDAP_DOWNGRADE_FIX_DIR}/ldap.bak ${LDAP_DOWNGRADE_FIX_DIR}/ldap_TO-BE-FIXED.bak
+  cat ${LDAP_DOWNGRADE_FIX_DIR}/ldap_TO-BE-FIXED.bak | grep -v -E '^ZIMBRA' > ${LDAP_DOWNGRADE_FIX_DIR}/ldap.bak
+
+  # Extra dumps not actually needed
+  /opt/zimbra/libexec/zmslapcat -c ${LDAP_DOWNGRADE_FIX_DIR}
+  /opt/zimbra/libexec/zmslapcat -a ${LDAP_DOWNGRADE_FIX_DIR}
+
+  # Stop ldap and remove old ldap database
+  ldap stop
+
+  mv /opt/zimbra/data/ldap/mdb /opt/zimbra/data/ldap/mdb.old_LDAP_DOWNGRADE_FIX
+  mkdir -p /opt/zimbra/data/ldap/mdb/db
+
+  # Import sanitized ldap dump
+
+  /opt/zimbra/libexec/zmslapadd ${LDAP_DOWNGRADE_FIX_DIR}/ldap.bak
 }
 
 if [ "x$(id -u -n)" != "xzimbra" ]; then
@@ -725,6 +749,11 @@ if [[ $restart_choice =~ ^[Yy]$ ]] ; then
   echo "Processing..."
   applyDowngrade /tmp/downgrade-ldap-entries-to-delete.ldif
   echo "...Done."
+
+  fixBorkedLdap
+
+  ldap stop
+  zmcontrol start
 else
   echo "Aborted by the user."
 fi
