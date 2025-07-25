4952b3105fdc68c2ca0a36d0c159a55889ea0999
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-07-25 12:57:55 +0200

    Added subscription links to advanced downloads page.

--- docs/generate-downloads-page.py
+++ docs/generate-downloads-page.py
@@ -375,21 +375,25 @@ def writeAdvancedDownloadsPage(downloads_md):
   outputNewLine(downloads_md)
   append_files(templatesDir + "/" + "stable-releases-top.md", downloads_md)
   append_files(templatesDir + "/" + "section-top-disclaimers.md", downloads_md)
+  append_files(templatesDir + "/" + "downloads-subscribe.md", downloads_md)
   outputSection(downloads_md=downloads_md, versionTags=stableVersionTags, releasesMatrix=stableReleasesMatrix, shortName='Stable')
 
   outputNewLine(downloads_md)
   append_files(templatesDir + "/" + "recent-releases-top.md", downloads_md)
   append_files(templatesDir + "/" + "section-top-disclaimers.md", downloads_md)
+  append_files(templatesDir + "/" + "downloads-subscribe.md", downloads_md)
   outputSection(downloads_md=downloads_md, versionTags=recentVersionTags, releasesMatrix=recentReleasesMatrix, shortName='Recent')
 
   outputNewLine(downloads_md)
   append_files(templatesDir + "/" + "experimental-releases-top.md", downloads_md)
   append_files(templatesDir + "/" + "section-top-disclaimers.md", downloads_md)
+  append_files(templatesDir + "/" + "downloads-subscribe.md", downloads_md)
   outputSection(downloads_md=downloads_md, versionTags=experimentalVersionTags, releasesMatrix=experimentalReleasesMatrix, shortName='Experimental')
 
   outputNewLine(downloads_md)
   append_files(templatesDir + "/" + "other-releases-top.md", downloads_md)
   append_files(templatesDir + "/" + "section-top-disclaimers.md", downloads_md)
+  append_files(templatesDir + "/" + "downloads-subscribe.md", downloads_md)
   outputSection(downloads_md=downloads_md, versionTags=otherVersionTags, releasesMatrix=otherReleasesMatrix, shortName='Other')
 
   outputNewLine(downloads_md)
