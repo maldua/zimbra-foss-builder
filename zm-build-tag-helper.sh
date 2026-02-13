c62f11a427a15bed3a2f744613b6f189d4d55266
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2026-02-13 19:40:57 +0100

    zm-build-tag-helper.sh: Add --pimbra-enabled support.

--- zm-build-tag-helper.sh
+++ zm-build-tag-helper.sh
@@ -3,21 +3,24 @@
 usage() {
   cat << EOF
 Usage:
-  $0 --git-default-tag <tags> --zm-build-branch-file <file> [--verbose]
+  $0 --git-default-tag <tags> --zm-build-branch-file <file> [--verbose] [--pimbra-enabled]
 
 Options:
   --git-default-tag        Comma-separated list of git tags to search (required)
   --zm-build-branch-file   Output file to write the found branch/tag (required)
+  --pimbra-enabled         Use maldua-pimbra/zm-build instead of Zimbra/zm-build
   --verbose                Show more information
   -h, --help               Show this help message
 
 Examples:
   $0 --git-default-tag '10.0.2,10.0.1,10.0.0-GA,10.0.0' --zm-build-branch-file zm-build-branch.txt
   $0 --verbose --git-default-tag '10.0.2,10.0.1,10.0.0-GA,10.0.0' --zm-build-branch-file zm-build-branch.txt
+  $0 --pimbra-enabled --git-default-tag '10.1.15.p1,10.0.2,10.0.1' --zm-build-branch-file zm-build-branch.txt
 EOF
 }
 
 GIT_QUIET="--quiet"
+PIMBRA_ENABLED=false
 
 # Parse arguments
 while [[ $# -gt 0 ]]; do
@@ -29,6 +32,10 @@ while [[ $# -gt 0 ]]; do
     --zm-build-branch-file)
       ZM_BUILD_BRANCH_FILE="$2"
       shift 2
+      ;;
+    --pimbra-enabled)
+      PIMBRA_ENABLED=true
+      shift
       ;;
     --verbose)
       GIT_QUIET=""
@@ -53,8 +60,16 @@ if [[ -z "$ZM_BUILD_GIT_DEFAULT_TAG" || -z "$ZM_BUILD_BRANCH_FILE" ]]; then
   exit 1
 fi
 
+# Select repository based on PIMBRA_ENABLED
+if $PIMBRA_ENABLED; then
+  ZM_BUILD_REPO="https://github.com/maldua-pimbra/zm-build"
+else
+  ZM_BUILD_REPO="https://github.com/Zimbra/zm-build"
+fi
+
 # Clone repo
-git clone ${GIT_QUIET} https://github.com/Zimbra/zm-build zm-build-find-branch
+git clone ${GIT_QUIET} ${ZM_BUILD_REPO} zm-build-find-branch
+
 cd zm-build-find-branch
 
 found_version=""
